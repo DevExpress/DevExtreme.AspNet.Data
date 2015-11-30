6e56e6febeba852d2a737d5a335c1ae5460aaabd
Aleksey Martynov <bclexa@gmail.com>
2015-11-30 12:09:59 +0300

    add replace-meta script

---
+++ build/replace-meta.ps1
@@ -0,0 +1,34 @@
+param ([string]$build_number, [string]$tag)
+
+$meta_version_numeric = "0.0"
+
+if ($tag -match '^v?(([.\d]+)[\w-]*)$') {
+    $meta_version_full = $matches[1]
+    $meta_version_numeric = $matches[2]
+} elseif ($build_number) {
+    $meta_version_full = "$meta_version_numeric-ci-build$build_number"
+} else {
+    $meta_version_full = $meta_version_numeric
+}
+
+$meta_company = "Developer Express Inc."
+$meta_copyright = "Copyright (c) $meta_company"
+$meta_description = "DevExtreme data layer extension for ASP.NET MVC"
+$meta_license_url = "https://raw.githubusercontent.com/DevExpress/DevExtreme.AspNet.Data/master/LICENSE"
+$meta_project_url = "https://github.com/DevExpress/DevExtreme.AspNet.Data"
+
+$targets = ("..\net\DevExtreme.AspNet.Data\AssemblyInfo.cs", "..\net\DevExtreme.AspNet.Data\project.json")
+
+$targets | %{
+    $path = "$PSScriptRoot\$_"
+
+    (Get-Content $path) | %{
+        $_  -replace '(AssemblyVersion.+?")[^"]+', "`${1}$meta_version_numeric" `
+            -replace '("version":.+?")[^"]+', "`${1}$meta_version_full" `
+            -replace '%meta_company%', $meta_company `
+            -replace '%meta_copyright%', $meta_copyright `
+            -replace '%meta_description%', $meta_description `
+            -replace '%meta_license_url%', $meta_license_url `
+            -replace '%meta_project_url%', $meta_project_url
+    } | Set-Content $path
+}
--- net/DevExtreme.AspNet.Data/AssemblyInfo.cs
+++ net/DevExtreme.AspNet.Data/AssemblyInfo.cs
@@ -4,5 +4,8 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("DevExtreme.AspNet.Data.Tests")]
 
 [assembly: AssemblyVersion("0.0")]
+[assembly: AssemblyCompany("%meta_company%")]
+[assembly: AssemblyCopyright("%meta_copyright%")]
+[assembly: AssemblyDescription("%meta_description%")]
 [assembly: AssemblyProduct("DevExtreme.AspNet.Data")]
-[assembly: AssemblyCopyright("Copyright (c) Developer Express Inc.")]
+
