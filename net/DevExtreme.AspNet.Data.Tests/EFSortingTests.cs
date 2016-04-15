942eb6e8d48b8f162a513ca4f6aab6b23052de94
AlekseyMartynov <bclexa@gmail.com>
2016-04-15 09:45:44 +0300

    spike solved

---
+++ net/DevExtreme.AspNet.Data.Tests/EFSortingTests.cs
@@ -0,0 +1,66 @@
+﻿using DevExtreme.AspNet.Data.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace System.ComponentModel.DataAnnotations {
+    class KeyAttribute : Attribute { }
+}
+
+namespace DevExtreme.AspNet.Data.Tests {
+
+    public class EFSortingTests {
+
+        class TestClass_Base {
+            protected int Bad1 = 0;
+            protected int Bad2 { get; set; }
+
+            public int Bad3 { get; }
+            public int Bad4 { get; internal set; }
+
+            public object Bad5 = null;
+            public object Bad6 { get; set; }
+        }
+
+        class TestClass_KeyAttr : TestClass_Base {
+            public int Skip { get; set; }
+
+            [System.ComponentModel.DataAnnotations.Key]
+            public int Key { get; set; }
+        }
+
+        class TestClass_LikelyKey : TestClass_Base {
+            public DateTime Skip { get; set; }
+            public Guid Key { get; set; }
+        }
+
+        class TestClass_Field : TestClass_Base {
+            public int Key = 0;
+        }
+
+        class TestClass_OtherSortable : TestClass_Base {
+            public SByte Sortable { get; set; }
+        }
+
+        class TestClass_Nullable : TestClass_Base {
+            public int? Sortable { get; set; }
+        }
+
+
+        [Fact]
+        public void FindSortableMember() {
+            Assert.Null(EFSorting.FindSortableMember(typeof(TestClass_Base)));
+            Assert.Equal("Key", EFSorting.FindSortableMember(typeof(TestClass_KeyAttr)));
+            Assert.Equal("Key", EFSorting.FindSortableMember(typeof(TestClass_LikelyKey)));
+            Assert.Equal("Key", EFSorting.FindSortableMember(typeof(TestClass_Field)));
+            Assert.Equal("Sortable", EFSorting.FindSortableMember(typeof(TestClass_OtherSortable)));
+            Assert.Equal("Sortable", EFSorting.FindSortableMember(typeof(TestClass_Nullable)));
+        }
+
+
+    }
+
+}
---
+++ net/DevExtreme.AspNet.Data/Helpers/EFSorting.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace DevExtreme.AspNet.Data.Helpers {
+
+    static class EFSorting {
+
+        static readonly ICollection<Type> LIKELY_KEY_TYPES = new[] {
+            typeof(Int32), typeof(Int64), typeof(Guid)
+        };
+
+        static readonly ICollection<Type> OTHER_SORTABLE_TYPES = new[] {
+            // https://msdn.microsoft.com/en-us/library/ee382832(v=vs.110).aspx
+            typeof(String), typeof(Boolean), typeof(DateTime), typeof(Decimal),
+            typeof(Double), typeof(DateTimeOffset), typeof(Byte), typeof(Single),
+            typeof(Int16), typeof(SByte)
+        };
+
+        class Candidate {
+            public readonly MemberInfo Member;
+            public readonly Type Type;
+
+            public Candidate(MemberInfo member, Type type) {
+                Member = member;
+                Type = Utils.StripNullableType(type);
+            }
+        }
+
+        public static string FindSortableMember(Type entityType) {
+            var candidates = Enumerable.Concat(
+                entityType.GetRuntimeProperties()
+                    .Where(i => i.CanRead && i.CanWrite && i.GetMethod.IsPublic && i.SetMethod.IsPublic)
+                    .Select(i => new Candidate(i, i.PropertyType)),
+                entityType.GetRuntimeFields()
+                    .Where(i => i.IsPublic)
+                    .Select(i => new Candidate(i, i.FieldType))
+            );
+
+            return (
+                candidates.FirstOrDefault(HasKeyAttr)
+                ?? candidates.FirstOrDefault(i => LIKELY_KEY_TYPES.Contains(i.Type))
+                ?? candidates.FirstOrDefault(i => OTHER_SORTABLE_TYPES.Contains(i.Type))
+            )?.Member.Name;
+        }
+
+        static bool HasKeyAttr(Candidate candidate) {
+            return candidate.Member.CustomAttributes.Any(i => i.AttributeType.FullName == "System.ComponentModel.DataAnnotations.KeyAttribute");
+        } 
+
+    }
+
+}
