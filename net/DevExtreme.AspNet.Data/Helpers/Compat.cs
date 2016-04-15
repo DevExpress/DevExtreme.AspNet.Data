dc69e5be896f0551568ff086ae69439f22f66972
AlekseyMartynov <bclexa@gmail.com>
2016-04-15 10:18:46 +0300

    use for EF

--- net/DevExtreme.AspNet.Data/Helpers/Compat.cs
+++ net/DevExtreme.AspNet.Data/Helpers/Compat.cs
@@ -14,6 +14,14 @@ namespace DevExtreme.AspNet.Data.Helpers {
             Expression<Func<DateTime>> closure = () => date;
             return closure.Body;
         }
+
+        internal static bool IsEntityFrramework(IQueryProvider provider) {
+            var type = provider.GetType().FullName;
+
+            return type == "System.Data.Entity.Internal.Linq.DbQueryProvider"
+                || type == "Microsoft.Data.Entity.Query.Internal.EntityQueryProvider";
+        }
+
     }
 
 }
