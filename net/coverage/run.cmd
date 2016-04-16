759cf5ad7725ccce91074cda1adf0ab99bb9f754
AlekseyMartynov <bclexa@gmail.com>
2016-04-16 18:19:17 +0300

    add scripts to calc test coverage

---
+++ net/coverage/run.cmd
@@ -0,0 +1,14 @@
+where dnx > dnxpath.txt
+set /p dnxpath=<dnxpath.txt
+
+call dnu build ..\DevExtreme.AspNet.Data.Tests
+
+OpenCover\tools\OpenCover.Console ^
+ "-target: %dnxpath%" ^
+ "-targetargs: --lib %~dp0\..\DevExtreme.AspNet.Data\bin\Debug\net45 -p ..\DevExtreme.AspNet.Data.Tests test" ^
+ "-filter:+[DevExtreme*]*" ^
+ -hideskipped:All -output:coverage.xml -register:user
+
+rd /s /q report
+ReportGenerator\tools\ReportGenerator -reports:coverage.xml -targetdir:report
+start report\index.htm
