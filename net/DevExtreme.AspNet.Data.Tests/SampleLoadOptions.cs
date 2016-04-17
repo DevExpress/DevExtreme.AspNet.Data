9f058f2e072d737b2e9f9b5cc8db8f5cb2d551f4
AlekseyMartynov <bclexa@gmail.com>
2016-04-17 09:49:41 +0300

    make expression log part of SampleLoadOptions

--- net/DevExtreme.AspNet.Data.Tests/SampleLoadOptions.cs
+++ net/DevExtreme.AspNet.Data.Tests/SampleLoadOptions.cs
@@ -6,9 +6,11 @@ using System.Threading.Tasks;
 namespace DevExtreme.AspNet.Data.Tests {
 
     class SampleLoadOptions : DataSourceLoadOptionsBase {
+        public List<string> ExpressionLog = new List<string>();
 
         public SampleLoadOptions() {
             UseQueryableOnce = true;
+            ExpressionWatcher = x => ExpressionLog.Add(x.ToString());
         }
 
     }
