903202a528570c5927b0d890557d9654630d6119
Aleksey Martynov <bclexa@gmail.com>
2016-05-18 18:35:53 +0300

    remove workaround for https://github.com/aspnet/EntityFramework/issues/3361

--- net/DevExtreme.AspNet.Data/Helpers/Compat.cs
+++ net/DevExtreme.AspNet.Data/Helpers/Compat.cs
@@ -7,13 +7,6 @@ using System.Threading.Tasks;
 namespace DevExtreme.AspNet.Data.Helpers {
 
     public static class Compat {
-        public static bool EF3361;
-
-#warning remove when https://github.com/aspnet/EntityFramework/issues/3361 is fixed (RC2)
-        internal static Expression Workaround_EF3361(DateTime date) {
-            Expression<Func<DateTime>> closure = () => date;
-            return closure.Body;
-        }
 
         internal static bool IsEntityFrramework(IQueryProvider provider) {
             var type = provider.GetType().FullName;
