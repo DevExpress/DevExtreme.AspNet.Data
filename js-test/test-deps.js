ff9b4e0dba10a97c402573030856acaf914eeeed
San4es <ziborov.sasha@gmail.com>
2016-06-29 15:47:30 +0300

    Update DevExtreme to 16.1.4

--- js-test/test-deps.js
+++ js-test/test-deps.js
@@ -1,5 +1,4 @@
 var TEST_DEPS = [
-    "bower_components/globalize/lib/globalize.js",
     "bower_components/jquery/dist/jquery.min.js",
     "bower_components/devextreme/js/dx.all.js",
     "bower_components/jquery-mockjax/dist/jquery.mockjax.min.js",
