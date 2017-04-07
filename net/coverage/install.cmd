98bc1d944b55f310dd49d5bb8eac8093844f9517
Aleksey Martynov <AlekseyMartynov@users.noreply.github.com>
2017-04-07 15:47:35 +0300

    drop old coverage

--- net/coverage/install.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-cd %~dp0
-
-if not exist nuget.exe (PowerShell -Command wget https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile nuget.exe)
-
-set NUGET_INSTALL=nuget install -ExcludeVersion -OutputDirectory .
-
-%NUGET_INSTALL% OpenCover
-%NUGET_INSTALL% ReportGenerator
