474e7e58be5a1f9565d8551770c6530d895a5715
Aleksey Martynov <AlekseyMartynov@users.noreply.github.com>
2017-05-04 15:24:55 +0300

    refactor

--- net/DevExtreme.AspNet.Data.Tests/DynamicBindingTests.cs
+++ net/DevExtreme.AspNet.Data.Tests/DynamicBindingTests.cs
@@ -1,81 +1,114 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Text;
 using Xunit;
 
 namespace DevExtreme.AspNet.Data.Tests {
 
     public class DynamicObjectTests {
+        const string P1 = "p1";
 
-        static IEnumerable<T> CreateTestData<T>() {
-            dynamic
-                item1 = new ExpandoObject(),
-                item2 = new ExpandoObject(),
-                item3 = new ExpandoObject();
-
-            item1.p = 1;
-            item2.p = 2;
-            item3.p = 3;
-
-
-            return new T[] { item3, item1, item2 };
+        static ExpandoObject CreateExpando(object p1) {
+            dynamic obj = new ExpandoObject();
+            obj.p1 = p1;
+            return obj;
         }
 
-        static T[] LoadDataOnly<T>(DataSourceLoadOptionsBase loadOptions) {
-            var opaqueResult = DataSourceLoader.Load(CreateTestData<T>(), loadOptions);
-            return ((IEnumerable<T>)opaqueResult).ToArray();
+        static IDictionary<string, object>[] ToDictArray(object obj) {
+            return ((IEnumerable)obj)
+                .Cast<IDictionary<string, object>>()
+                .ToArray();
         }
 
         [Fact]
         public void Sort() {
             var loadOptions = new SampleLoadOptions {
                 Sort = new[] {
-                    new SortingInfo { Selector = "p" }
+                    new SortingInfo { Selector = P1 }
                 }
             };
+            
+            var resultObject = ToDictArray(DataSourceLoader.Load(
+                new object[] {
+                    CreateExpando(2),
+                    CreateExpando(1)
+                }, 
+                loadOptions
+            ));
 
-            dynamic resultDynamic = LoadDataOnly<dynamic>(loadOptions);
-            dynamic resultExpando = LoadDataOnly<ExpandoObject>(loadOptions);
+            var resultExpando = ToDictArray(DataSourceLoader.Load(
+                new[] {
+                    CreateExpando(2),
+                    CreateExpando(1)
+                }, 
+                loadOptions
+            ));
 
-            Assert.Equal(1, resultDynamic[0].p);
-            Assert.Equal(3, resultDynamic[2].p);
+            Assert.Equal(1, resultObject[0][P1]);
+            Assert.Equal(2, resultObject[1][P1]);
 
-            Assert.Equal(1, resultExpando[0].p);
-            Assert.Equal(3, resultExpando[2].p);
+            Assert.Equal(1, resultExpando[0][P1]);
+            Assert.Equal(2, resultExpando[1][P1]);
         }
 
         [Fact]
         public void Filter() {
             var loadOptions = new SampleLoadOptions {
-                Filter = new object[] { "p", ">", 2 }
+                Filter = new object[] { P1, ">", 1 }
             };
 
-            dynamic resultDynamic = LoadDataOnly<dynamic>(loadOptions);
-            dynamic resultExpando = LoadDataOnly<ExpandoObject>(loadOptions);
+            var resultObject = ToDictArray(DataSourceLoader.Load(
+                new object[] {
+                    CreateExpando(1),
+                    CreateExpando(2)
+                },
+                loadOptions
+            ));
 
-            Assert.Equal(1, resultDynamic.Length);
-            Assert.Equal(3, resultDynamic[0].p);
+            var resultExpando = ToDictArray(DataSourceLoader.Load(
+                new[] {
+                    CreateExpando(1),
+                    CreateExpando(2)
+                },
+                loadOptions
+            ));
+
+            Assert.Equal(1, resultObject.Length);
+            Assert.Equal(2, resultObject[0][P1]);
 
             Assert.Equal(1, resultExpando.Length);
-            Assert.Equal(3, resultExpando[0].p);
+            Assert.Equal(2, resultExpando[0][P1]);
         }
 
         [Fact]
         public void TotalSummary() {
             var loadOptions = new SampleLoadOptions {
                 TotalSummary = new[] {
-                    new SummaryInfo { Selector = "p", SummaryType = "sum" }
+                    new SummaryInfo { Selector = P1, SummaryType = "sum" }
                 }
             };
 
-            var resultDynamic = (DataSourceLoadResult)DataSourceLoader.Load(CreateTestData<dynamic>(), loadOptions);
-            var resultExpando = (DataSourceLoadResult)DataSourceLoader.Load(CreateTestData<ExpandoObject>(), loadOptions);
+            var resultObject = (DataSourceLoadResult)DataSourceLoader.Load(
+                new object[] {
+                    CreateExpando(1),
+                    CreateExpando(2)
+                },
+                loadOptions
+            );
 
-            Assert.Equal(6m, resultDynamic.summary[0]);
-            Assert.Equal(6m, resultExpando.summary[0]);
+            var resultExpando = (DataSourceLoadResult)DataSourceLoader.Load(
+                new[] {
+                    CreateExpando(1),
+                    CreateExpando(2)
+                },
+                loadOptions
+            );
+
+            Assert.Equal(3m, resultObject.summary[0]);
+            Assert.Equal(3m, resultExpando.summary[0]);
         }
 
     }
