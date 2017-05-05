a74f7d19880941954424ad6c3c6236be44483586
AlekseyMartynov <AlekseyMartynov@users.noreply.github.com>
2017-05-06 00:49:05 +0300

    Use version-independent LocalDB

--- net/DevExtreme.AspNet.Data.Tests.EF6/TestDbContext.cs
+++ net/DevExtreme.AspNet.Data.Tests.EF6/TestDbContext.cs
@@ -12,7 +12,7 @@ namespace DevExtreme.AspNet.Data.Tests.EF6 {
         static TestDbContext INSTANCE;
 
         private TestDbContext(params Type[] knownEntities)
-            : base($"Data Source=(localdb)\\v11.0; Initial Catalog={typeof(TestDbContext).Assembly.GetName().Name}; Integrated Security=True") {
+            : base($"Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog={typeof(TestDbContext).Assembly.GetName().Name}; Integrated Security=True") {
         }
 
         public static void Exec(Action<TestDbContext> action) {
