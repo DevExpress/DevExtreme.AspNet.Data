b3b7eb0925c2d4d99d6116d5e528e728ee023990
Aleksey Martynov <AlekseyMartynov@users.noreply.github.com>
2017-11-28 16:17:04 +0300

    mute

--- net/docfx/build.cmd
+++ net/docfx/build.cmd
@@ -8,7 +8,7 @@ where nuget || (
     if not exist build/nuget.exe (PowerShell -Command wget https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -O build/nuget.exe)
 )
 
-nuget install -ExcludeVersion -OutputDirectory build docfx.console -Version 2.27.0
-nuget install -ExcludeVersion -OutputDirectory build msdn.4.5.2 -Pre
+nuget install -ExcludeVersion -OutputDirectory build -Verbosity q docfx.console -Version 2.27.0
+nuget install -ExcludeVersion -OutputDirectory build -Verbosity q msdn.4.5.2 -Pre
 
 build\docfx.console\tools\docfx
