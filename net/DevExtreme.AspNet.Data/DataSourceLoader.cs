e9ab8f46ddd338c9504ebdc73957e04b64bd2b75
Aleksey Martynov <AlekseyMartynov@users.noreply.github.com>
2017-11-28 11:23:44 +0300

    Sample doc comments

--- net/DevExtreme.AspNet.Data/DataSourceLoader.cs
+++ net/DevExtreme.AspNet.Data/DataSourceLoader.cs
@@ -7,6 +7,13 @@ namespace DevExtreme.AspNet.Data {
 
     public class DataSourceLoader {
 
+        /// <summary>
+        /// Sample description
+        /// </summary>
+        /// <typeparam name="T">Sample description</typeparam>
+        /// <param name="source">Sample description</param>
+        /// <param name="options">Sample description</param>
+        /// <returns>Sample description</returns>
         public static LoadResult Load<T>(IEnumerable<T> source, DataSourceLoadOptionsBase options) {
             return Load(source.AsQueryable(), options);
         }
