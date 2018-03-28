e2528461914f48fbc60c5c21813034fdfc88a94a
Aleksey Martynov <AlekseyMartynov@users.noreply.github.com>
2018-03-28 16:22:11 +0300

    Fix compatibility with the latest xhr-mock

--- js-test/test-deps.js
+++ js-test/test-deps.js
@@ -1,4 +1,5 @@
 var TEST_DEPS = [
+    "ie-stubs.js",
     "../node_modules/jquery/dist/jquery.min.js",
     "../node_modules/devextreme/dist/js/dx.all.js",
     "../node_modules/xhr-mock/dist/xhr-mock.js",
