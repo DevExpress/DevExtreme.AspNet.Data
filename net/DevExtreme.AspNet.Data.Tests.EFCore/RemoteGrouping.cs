289fa2ec8b582ff0e7f956d3659157f3b70b04ec
Aleksey Martynov <AlekseyMartynov@users.noreply.github.com>
2018-04-25 20:26:19 +0300

    Check that RemoteGrouping defaults to False with EF Core

---
+++ net/DevExtreme.AspNet.Data.Tests.EFCore/RemoteGrouping.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace DevExtreme.AspNet.Data.Tests.EFCore {
+
+    class RemoteGrouping_DataItem {
+        public int ID { get; set; }
+        public int Group { get; set; }
+    }
+
+    partial class TestDbContext {
+        public DbSet<RemoteGrouping_DataItem> RemoteGrouping_Data { get; set; }
+    }
+
+    public class RemoteGrouping {
+
+        [Fact]
+        public void DisabledByDefault() {
+            TestDbContext.Exec(context => {
+                var dbSet = context.RemoteGrouping_Data;
+                var exprLog = new List<string>();
+
+                var loadOptions = new SampleLoadOptions {
+                    Group = new[] {
+                        new GroupingInfo {
+                            Selector = "Group",
+                            IsExpanded = false
+                        }
+                    },
+                    ExpressionWatcher = x => exprLog.Add(x.ToString())
+                };
+
+                Assert.DoesNotContain(exprLog, i => i.Contains(".GroupBy"));
+            });
+        }
+
+    }
+
+}
