ffa0f319fbb93ced6abdd3e119074856ab09a24a
Aleksey Martynov <AlekseyMartynov@users.noreply.github.com>
2018-04-25 17:40:19 +0300

    https://github.com/aspnet/EntityFramework6/issues/384#issuecomment-372510711

--- net/DevExtreme.AspNet.Data.Tests.EF6/TestDbContext.cs
+++ net/DevExtreme.AspNet.Data.Tests.EF6/TestDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,11 @@ namespace DevExtreme.AspNet.Data.Tests.EF6 {
 
     partial class TestDbContext : DbContext {
         static readonly object LOCK = new object();
+        static readonly string DB_FILE_PATH = Path.Combine(Path.GetTempPath(), typeof(TestDbContext).Assembly.GetName().Name + ".mdf");
         static TestDbContext INSTANCE;
 
         private TestDbContext()
-            : base($"Data Source=(localdb)\\MSSQLLocalDB; Initial Catalog={typeof(TestDbContext).Assembly.GetName().Name}; Integrated Security=True") {
+            : base($"Data Source=(localdb)\\MSSQLLocalDB; AttachDbFileName={DB_FILE_PATH}; Integrated Security=True") {
         }
 
         public static void Exec(Action<TestDbContext> action) {
