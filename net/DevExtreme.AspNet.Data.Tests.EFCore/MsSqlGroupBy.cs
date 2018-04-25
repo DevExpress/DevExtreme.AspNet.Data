5d26b9f2374968b118e012e76ceea8124cf5a3b0
Aleksey Martynov <AlekseyMartynov@users.noreply.github.com>
2018-04-25 19:36:34 +0300

    TestDbContext for EF Core

---
+++ net/DevExtreme.AspNet.Data.Tests.EFCore/MsSqlGroupBy.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DevExtreme.AspNet.Data.Tests.EFCore {
+
+    class MsSqlGroupBy_DataItem {
+        public int ID { get; set; }
+        public int Group { get; set; }
+    }
+
+    partial class TestDbContext {
+        public DbSet<MsSqlGroupBy_DataItem> MsSqlGroupBy_Data { get; set; }
+    }
+
+    public class MsSqlGroupBy {
+
+        [Fact]
+        public void Scenario() {
+            TestDbContext.Exec(context => {
+                var dbSet = context.MsSqlGroupBy_Data;
+
+                dbSet.AddRange(
+                    new MsSqlGroupBy_DataItem { Group = 1 },
+                    new MsSqlGroupBy_DataItem { Group = 1 }
+                );
+
+                context.SaveChanges();
+
+#warning TODO
+            });
+        }
+
+    }
+
+}
---
+++ net/DevExtreme.AspNet.Data.Tests.EFCore/TestDbContext.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Reflection;
+
+namespace DevExtreme.AspNet.Data.Tests.EFCore {
+
+    partial class TestDbContext : DbContext {
+        static readonly object LOCK = new object();
+        static TestDbContext INSTANCE;
+
+        static readonly string DB_NAME = typeof(TestDbContext).GetTypeInfo().Assembly.GetName().Name;
+        static readonly string DB_FILE_PATH = Path.Combine(Path.GetTempPath(), DB_NAME + ".mdf");
+
+
+        private TestDbContext() {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) {
+            options.UseSqlServer($"Data Source=(localdb)\\MSSQLLocalDB; AttachDbFileName={DB_FILE_PATH}; Initial Catalog={DB_NAME}");
+        }
+
+        public static void Exec(Action<TestDbContext> action) {
+            lock(LOCK) {
+                if(INSTANCE == null) {
+                    ResetTestDatabase();
+                    INSTANCE = new TestDbContext();                    
+                    INSTANCE.Database.EnsureCreated();
+                }
+
+                action(INSTANCE);
+            }
+        }
+
+        static void ResetTestDatabase() {
+            // Possibly related: https://stackoverflow.com/a/46142857
+
+            using(var conn = new SqlConnection($"Data Source=(localdb)\\MSSQLLocalDB")) {
+                conn.Open();
+
+                void Exec(string sql) {
+                    using(var cmd = conn.CreateCommand()) {
+                        cmd.CommandText = sql;
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+
+
+                try {
+                    Exec($"drop database [{DB_NAME}]");
+                } catch {
+                }
+
+                Exec($"create database [{DB_NAME}] on (name='{DB_NAME}', filename='{DB_FILE_PATH}')");
+            }
+        }
+    }
+
+}
