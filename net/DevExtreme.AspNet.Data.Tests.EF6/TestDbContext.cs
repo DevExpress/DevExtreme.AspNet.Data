2942c0baf976266d990bcad1f0a7a85f808d5230
Aleksey Martynov <AlekseyMartynov@users.noreply.github.com>
2018-05-03 15:37:40 +0300

    Extract SqlServerTestDbHelper

--- net/DevExtreme.AspNet.Data.Tests.EF6/TestDbContext.cs
+++ net/DevExtreme.AspNet.Data.Tests.EF6/TestDbContext.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,18 +9,19 @@ namespace DevExtreme.AspNet.Data.Tests.EF6 {
 
     partial class TestDbContext : DbContext {
         static readonly object LOCK = new object();
-        static readonly string DB_FILE_PATH = Path.Combine(Path.GetTempPath(), typeof(TestDbContext).Assembly.GetName().Name + ".mdf");
         static TestDbContext INSTANCE;
 
-        private TestDbContext()
-            : base($"Data Source=(localdb)\\MSSQLLocalDB; AttachDbFileName={DB_FILE_PATH}; Integrated Security=True") {
+        private TestDbContext(string connectionString)
+            : base(connectionString) {
         }
 
         public static void Exec(Action<TestDbContext> action) {
             lock(LOCK) {
                 if(INSTANCE == null) {
-                    INSTANCE = new TestDbContext();
-                    INSTANCE.Database.Delete();
+                    var helper = new SqlServerTestDbHelper("DevExtreme_AspNet_Data_Tests_EF6_DB");
+                    helper.ResetDatabase();
+
+                    INSTANCE = new TestDbContext(helper.ConnectionString);
                     INSTANCE.Database.CreateIfNotExists();
                 }
 
--- net/DevExtreme.AspNet.Data.Tests.EFCore1/TestDbContext.cs
+++ net/DevExtreme.AspNet.Data.Tests.EFCore1/TestDbContext.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.IO;
-using System.Reflection;
 
 namespace DevExtreme.AspNet.Data.Tests.EFCore1 {
 
@@ -11,22 +8,21 @@ namespace DevExtreme.AspNet.Data.Tests.EFCore1 {
         static readonly object LOCK = new object();
         static TestDbContext INSTANCE;
 
-        static readonly string DB_NAME = typeof(TestDbContext).GetTypeInfo().Assembly.GetName().Name;
-        static readonly string DB_FILE_PATH = Path.Combine(Path.GetTempPath(), DB_NAME + ".mdf");
-
-
-        private TestDbContext() {
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder options) {
-            options.UseSqlServer($"Data Source=(localdb)\\MSSQLLocalDB; AttachDbFileName={DB_FILE_PATH}; Initial Catalog={DB_NAME}");
+        private TestDbContext(DbContextOptions options)
+            : base(options) {
         }
 
         public static void Exec(Action<TestDbContext> action) {
             lock(LOCK) {
                 if(INSTANCE == null) {
-                    ResetTestDatabase();
-                    INSTANCE = new TestDbContext();
+                    var helper = new SqlServerTestDbHelper("DevExtreme_AspNet_Data_Tests_EFCore1_DB");
+                    helper.ResetDatabase();
+
+                    var options = new DbContextOptionsBuilder()
+                        .UseSqlServer(helper.ConnectionString)
+                        .Options;
+
+                    INSTANCE = new TestDbContext(options);
                     INSTANCE.Database.EnsureCreated();
                 }
 
@@ -34,28 +30,6 @@ namespace DevExtreme.AspNet.Data.Tests.EFCore1 {
             }
         }
 
-        static void ResetTestDatabase() {
-            // Possibly related: https://stackoverflow.com/a/46142857
-
-            using(var conn = new SqlConnection($"Data Source=(localdb)\\MSSQLLocalDB")) {
-                conn.Open();
-
-                void Exec(string sql) {
-                    using(var cmd = conn.CreateCommand()) {
-                        cmd.CommandText = sql;
-                        cmd.ExecuteNonQuery();
-                    }
-                }
-
-
-                try {
-                    Exec($"drop database [{DB_NAME}]");
-                } catch {
-                }
-
-                Exec($"create database [{DB_NAME}] on (name='{DB_NAME}', filename='{DB_FILE_PATH}')");
-            }
-        }
     }
 
 }
