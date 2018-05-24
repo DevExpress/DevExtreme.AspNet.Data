6eebfc6be596c62a4fb455bd8ff9adbc69e66f46
Aleksey Martynov <AlekseyMartynov@users.noreply.github.com>
2018-05-24 17:41:06 +0300

    XPO test project + red case

---
+++ net/DevExtreme.AspNet.Data.Tests.Xpo/Tests.cs
@@ -0,0 +1,30 @@
+﻿using DevExpress.Xpo;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace DevExtreme.AspNet.Data.Tests.Xpo {
+
+    public class Tests {
+
+        [Fact]
+        public void DefaultSort_Projection() {
+            UnitOfWorkHelper.Exec(uow => {
+                new GenericTestEntity(uow) { Text = "a" };
+                new GenericTestEntity(uow) { Text = "c" };
+                new GenericTestEntity(uow) { Text = "b" };
+                uow.CommitChanges();
+
+                var projection = uow.Query<GenericTestEntity>().Select(i => new { i.Text });
+                var loadResult = DataSourceLoader.Load(projection, new SampleLoadOptions {
+                    Skip = 1,
+                    Take = 1
+                });
+
+                var data = loadResult.data.Cast<object>().ToArray();
+#warning TODO
+            });
+        }
+    }
+
+}
---
+++ net/DevExtreme.AspNet.Data.Tests.Xpo/UnitOfWorkHelper.cs
@@ -0,0 +1,41 @@
+﻿using DevExpress.Xpo;
+using DevExpress.Xpo.DB;
+using DevExpress.Xpo.Metadata;
+using System;
+
+namespace DevExtreme.AspNet.Data.Tests.Xpo {
+
+    class UnitOfWorkHelper {
+        static readonly object LOCK = new object();
+        static IDataLayer DATA_LAYER;
+
+        public static void Exec(Action<UnitOfWork> action) {
+            XpoDefault.Session = null;
+
+            lock(LOCK) {
+                if(DATA_LAYER == null) {
+                    var dict = new ReflectionDictionary();
+                    dict.GetDataStoreSchema(
+                        typeof(GenericTestEntity)
+                    );
+
+                    var provider = XpoDefault.GetConnectionProvider(
+                        new SqlServerTestDbHelper("DevExtreme_AspNet_Data_Tests_Xpo_DB").ConnectionString,
+                        AutoCreateOption.DatabaseAndSchema
+                    );
+
+                    DATA_LAYER = new SimpleDataLayer(dict, provider);
+                }
+
+                using(var purger = new Session(DATA_LAYER)) {
+                    purger.ExecuteNonQuery("delete from " + nameof(GenericTestEntity));
+                }
+
+                using(var uow = new UnitOfWork(DATA_LAYER)) {
+                    action(uow);
+                }
+            }
+        }
+    }
+
+}
