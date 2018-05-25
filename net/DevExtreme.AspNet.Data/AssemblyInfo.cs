d791d593f88a9fafc46aeb976991be60955ca547
AlekseyMartynov <AlekseyMartynov@users.noreply.github.com>
2018-05-25 12:58:19 +0300

    Truncate unused InternalsVisibleTo

--- net/DevExtreme.AspNet.Data/AssemblyInfo.cs
+++ net/DevExtreme.AspNet.Data/AssemblyInfo.cs
@@ -5,9 +5,6 @@ using System.Runtime.CompilerServices;
 #if DEBUG
 [assembly: InternalsVisibleTo("DevExtreme.AspNet.Data.Tests")]
 [assembly: InternalsVisibleTo("DevExtreme.AspNet.Data.Tests.Common")]
-[assembly: InternalsVisibleTo("DevExtreme.AspNet.Data.Tests.EF6")]
-[assembly: InternalsVisibleTo("DevExtreme.AspNet.Data.Tests.EFCore1")]
-[assembly: InternalsVisibleTo("DevExtreme.AspNet.Data.Tests.EFCore2")]
 #endif
 
 [assembly: CLSCompliant(true)]
