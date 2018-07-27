6c2f9ad1f1369ddcc99c6d3ee46313421ca26767
Aleksey Martynov <AlekseyMartynov@users.noreply.github.com>
2018-07-27 15:23:46 +0300

    Add owner to CI versions

--- build/replace-meta.js
+++ build/replace-meta.js
@@ -5,23 +5,29 @@
 const fs = require("fs");
 const path = require("path");
 
+const MAIN_REPO_NAME = "DevExpress/DevExtreme.AspNet.Data";
+
 const META_COMPANY = "Developer Express Inc.";
 const META_COPYRIGHT = "Copyright (c) " + META_COMPANY;
 const META_DESCRIPTION = "DevExtreme data layer extension for ASP.NET";
-const META_LICENSE_URL = "https://raw.githubusercontent.com/DevExpress/DevExtreme.AspNet.Data/master/LICENSE";
-const META_PROJECT_URL = "https://github.com/DevExpress/DevExtreme.AspNet.Data";
+const META_LICENSE_URL = `https://raw.githubusercontent.com/${MAIN_REPO_NAME}/master/LICENSE`;
+const META_PROJECT_URL = "https://github.com/" + MAIN_REPO_NAME;
 
 const BUILD_NUMBER = process.argv[2];
 const TAG = process.argv[3];
+const REPO_NAME = process.argv[4];
 
 let META_VERSION_NUMERIC = "99.0.0";
 let META_VERSION_FULL;
 
-if(/^v?(([.\d]+)[\w-]*)$/.test(TAG)) {
+if(REPO_NAME == MAIN_REPO_NAME && /^v?(([.\d]+)[\w-]*)$/.test(TAG)) {
     META_VERSION_FULL = RegExp.$1;
     META_VERSION_NUMERIC = RegExp.$2;
 } else if(BUILD_NUMBER) {
     META_VERSION_FULL = META_VERSION_NUMERIC + "-ci-";
+
+    if(REPO_NAME && REPO_NAME !== MAIN_REPO_NAME)
+        META_VERSION_FULL += REPO_NAME.split("/")[0].toLowerCase() + "-";
 
     for(let i = 0; i < 6 - BUILD_NUMBER.length; i++)
         META_VERSION_FULL += "0";
