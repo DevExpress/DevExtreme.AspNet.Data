42143f028758c0d9f5e22b454cbd9566e9fa9fe9
Alexander Ziborov <1420883+San4es@users.noreply.github.com>
2018-08-17 16:35:38 +0300

    Tests: Replace CommaAggregator with TotalSalesAggregator (#282)

---
+++ net/DevExtreme.AspNet.Data.Tests/TotalSalesAggregator.cs
@@ -0,0 +1,29 @@
+﻿using DevExtreme.AspNet.Data.Aggregation;
+using DevExtreme.AspNet.Data.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DevExtreme.AspNet.Data.Tests {
+
+    #region class
+    class TotalSalesAggregator<T> : Aggregator<T> {
+        decimal _total = 0;
+
+        public TotalSalesAggregator(IAccessor<T> accessor)
+            : base(accessor) {
+        }
+
+        public override void Step(T container, string selector) {
+            var quantity = Convert.ToInt32(Accessor.Read(container, "Quantity"));
+            var unitPrice = Convert.ToDecimal(Accessor.Read(container, "UnitPrice"));
+            var discount = Convert.ToDecimal(Accessor.Read(container, "Discount"));
+            _total += quantity * unitPrice * (1 - discount);
+        }
+
+        public override object Finish() {
+            return _total;
+        }
+    }
+    #endregion
+}
