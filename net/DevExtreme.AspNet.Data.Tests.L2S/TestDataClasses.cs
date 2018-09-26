b7c24c83d9041766078b496d04b285ae85037d8e
AlekseyMartynov <AlekseyMartynov@users.noreply.github.com>
2018-09-26 19:33:34 +0300

    LINQ 2 SQL compatibility (#293)

---
+++ net/DevExtreme.AspNet.Data.Tests.L2S/TestDataClasses.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace DevExtreme.AspNet.Data.Tests.L2S {
+
+    partial class TestDataContext {
+        static readonly object LOCK = new object();
+        static TestDataContext INSTANCE;
+
+        public static void Exec(Action<TestDataContext> action) {
+            lock(LOCK) {
+                if(INSTANCE == null) {
+                    var helper = new SqlServerTestDbHelper("L2S");
+                    helper.ResetDatabase();
+
+                    INSTANCE = new TestDataContext(helper.ConnectionString);
+
+                    INSTANCE.ExecuteCommand(
+                        $@"create table {nameof(RemoteGroupingStress_DataItem)} (
+                            {nameof(RemoteGroupingStress_DataItem.ID)} int identity primary key,
+                            {nameof(RemoteGroupingStress_DataItem.Num)} int not null,
+                            {nameof(RemoteGroupingStress_DataItem.NullNum)} int,
+                            {nameof(RemoteGroupingStress_DataItem.Date)} datetime2 not null,
+                            {nameof(RemoteGroupingStress_DataItem.NullDate)} datetime2
+                        )"
+                    );
+
+                    INSTANCE.ExecuteCommand(
+                        $@"create table {nameof(Summary_DataItem)} (
+                            {nameof(Summary_DataItem.ID)} int identity primary key,
+                            {nameof(Summary_DataItem.Group1)} nvarchar,
+                            {nameof(Summary_DataItem.Group2)} nvarchar,
+                            {nameof(Summary_DataItem.Value)} int
+                        )"
+                    );
+                }
+
+                action(INSTANCE);
+            }
+        }
+
+    }
+
+    partial class RemoteGroupingStress_DataItem : RemoteGroupingStressHelper.IEntity {
+    }
+
+    partial class Summary_DataItem : SummaryTestHelper.IEntity {
+    }
+
+}
