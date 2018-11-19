688268fc3373757bf8f914ee5feed3c37be376ed
Aleksey Martynov <AlekseyMartynov@users.noreply.github.com>
2018-11-19 13:07:53 +0300

    Extract SelectHelper

--- net/DevExtreme.AspNet.Data/DataSourceLoaderImpl.cs
+++ net/DevExtreme.AspNet.Data/DataSourceLoaderImpl.cs
@@ -6,10 +6,8 @@ using DevExtreme.AspNet.Data.Types;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Dynamic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
 
 namespace DevExtreme.AspNet.Data {
 
@@ -98,7 +96,7 @@ namespace DevExtreme.AspNet.Data {
 
                 if(Options.HasAnySelect) {
                     ContinueWithGrouping(
-                        ExecWithSelect(loadExpr).Select(ProjectionToExpando),
+                        ExecWithSelect(loadExpr),
                         result
                     );
                 } else {
@@ -118,13 +116,15 @@ namespace DevExtreme.AspNet.Data {
             return result;
         }
 
-        IEnumerable<AnonType> ExecWithSelect(Expression loadExpr) {
+        IEnumerable<IDictionary<string, object>> ExecWithSelect(Expression loadExpr) {
             if(Options.UseRemoteSelect)
-                return ExecExpr<AnonType>(Source, loadExpr);
+                return SelectHelper.ConvertRemoteResult(ExecExpr<AnonType>(Source, loadExpr), Options.GetFullSelect());
 
             var inMemoryQuery = ForceExecution(ExecExpr<S>(Source, loadExpr)).AsQueryable();
             var selectExpr = new SelectExpressionCompiler<S>(true).Compile(inMemoryQuery.Expression, Options.GetFullSelect());
-            return ExecExpr<AnonType>(inMemoryQuery, selectExpr);
+
+            // TODO
+            return SelectHelper.ConvertRemoteResult(ExecExpr<AnonType>(inMemoryQuery, selectExpr), Options.GetFullSelect());
         }
 
         void ContinueWithGrouping<R>(IEnumerable<R> loadResult, LoadResult result) {
@@ -236,30 +236,6 @@ namespace DevExtreme.AspNet.Data {
                 } else {
                     EmptyGroups(g.items, level - 1);
                 }
-            }
-        }
-
-        ExpandoObject ProjectionToExpando(AnonType projection) {
-            var expando = new ExpandoObject();
-            var index = 0;
-            foreach(var name in Options.GetFullSelect()) {
-                ShrinkSelectResult(expando, name.Split('.'), projection[index]);
-                index++;
-            }
-            return expando;
-        }
-
-        static void ShrinkSelectResult(IDictionary<string, object> target, string[] path, object value, int index = 0) {
-            var key = path[index];
-
-            if(index == path.Length - 1) {
-                target[key] = value;
-            } else {
-                if(!target.ContainsKey(key))
-                    target[key] = new ExpandoObject();
-
-                if(target[key] is IDictionary<string, object> child)
-                    ShrinkSelectResult(child, path, value, 1 + index);
             }
         }
     }
---
+++ net/DevExtreme.AspNet.Data/SelectHelper.cs
@@ -0,0 +1,40 @@
+﻿using DevExtreme.AspNet.Data.Types;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+
+namespace DevExtreme.AspNet.Data {
+
+    static class SelectHelper {
+
+        public static IEnumerable<IDictionary<string, object>> ConvertRemoteResult(IEnumerable<AnonType> selectResult, IEnumerable<string> names) {
+            var paths = names.Select(n => n.Split('.')).ToArray();
+
+            foreach(var anonObj in selectResult) {
+                var expando = new ExpandoObject();
+
+                for(var i = 0; i < paths.Length; i++)
+                    Shrink(expando, paths[i], anonObj[i]);
+
+                yield return expando;
+            }
+        }
+
+        static void Shrink(IDictionary<string, object> target, string[] path, object value, int index = 0) {
+            var key = path[index];
+
+            if(index == path.Length - 1) {
+                target[key] = value;
+            } else {
+                if(!target.ContainsKey(key))
+                    target[key] = new ExpandoObject();
+
+                if(target[key] is IDictionary<string, object> child)
+                    Shrink(child, path, value, 1 + index);
+            }
+        }
+
+    }
+
+}
