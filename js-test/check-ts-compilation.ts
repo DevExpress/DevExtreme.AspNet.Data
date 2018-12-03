3d9edf2878ab543cd46e761407f735f7033267d7
AlekseyMartynov <AlekseyMartynov@users.noreply.github.com>
2018-12-03 20:00:30 +0300

    Support errorHandler option in createStore (#312)

--- js-test/check-ts-compilation.ts
+++ js-test/check-ts-compilation.ts
@@ -2,6 +2,7 @@ import * as AspNetData from '../js/dx.aspnet.data'
 
 let store = AspNetData.createStore({
     key: 'id',
+    errorHandler: e => alert(e.message),
     loadUrl: "http://example.com",
     onBeforeSend: (operation, ajax) => {
         ajax.headers['X-Custom'] = 'value'
