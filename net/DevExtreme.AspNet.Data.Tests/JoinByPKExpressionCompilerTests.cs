4d8b33ef568566df2ae2851254aa30796424e6dc
Aleksey Martynov <AlekseyMartynov@users.noreply.github.com>
2019-03-22 15:11:24 +0300

    JoinByPKExpressionCompiler

---
+++ net/DevExtreme.AspNet.Data.Tests/JoinByPKExpressionCompilerTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace DevExtreme.AspNet.Data.Tests {
+
+    public class JoinByPKExpressionCompilerTests {
+
+        class DataItem {
+            public int K1 { get; set; }
+            public long K2 { get; set; }
+            public string Prop { get; set; }
+        }
+
+        static readonly IQueryable<DataItem> SOURCE = new DataItem[0].AsQueryable();
+
+        [Fact]
+        public void MultiKey() {
+            var compiler = new JoinByPKExpressionCompiler<DataItem>(false, null);
+
+            var expr = compiler.Compile(
+                SOURCE.Expression,
+                SOURCE.Skip(1).Take(2).Expression,
+                new[] { "K1", "K2" }
+            );
+
+            Assert.Equal(typeof(IQueryable<DataItem>), expr.Type);
+
+            Assert.Equal(
+                "source.Join(" +
+                    "source.Skip(1).Take(2), " +
+                    "obj => new AnonType`2(I0 = obj.K1, I1 = obj.K2), " +
+                    "obj => new AnonType`2(I0 = obj.K1, I1 = obj.K2), " +
+                    "(outer, inner) => outer" +
+                ")",
+                ShortenSource(expr.ToString())
+            );
+        }
+
+        [Fact]
+        public void SingleKey() {
+            var compiler = new JoinByPKExpressionCompiler<DataItem>(false, null);
+
+            var expr = compiler.Compile(
+                SOURCE.Expression,
+                SOURCE.Expression,
+                new[] { "K1" }
+            );
+
+            var exprText = expr.ToString();
+
+            Assert.DoesNotContain("AnonType", exprText);
+            Assert.Contains("obj => obj.K1", exprText);
+        }
+
+
+        static string ShortenSource(string expr) {
+            return expr.Replace(typeof(DataItem).FullName + "[]", "source");
+        }
+
+    }
+
+}
---
+++ net/DevExtreme.AspNet.Data/JoinByPKExpressionCompiler.cs
@@ -0,0 +1,47 @@
+﻿using DevExtreme.AspNet.Data.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DevExtreme.AspNet.Data {
+
+    class JoinByPKExpressionCompiler<T> : ExpressionCompiler {
+        AnonTypeNewTweaks _anonTypeNewTweaks;
+
+        public JoinByPKExpressionCompiler(bool guardNulls, AnonTypeNewTweaks anonTypeNewTweaks)
+            : base(guardNulls) {
+            _anonTypeNewTweaks = anonTypeNewTweaks;
+        }
+
+        public Expression Compile(Expression outer, Expression inner, IReadOnlyList<string> pk) {
+            var dataItemExpr = CreateItemParam(typeof(T));
+
+            var pkParam = CreateItemParam(typeof(T));
+            var pkLambda = Expression.Lambda(
+                pk.Count < 2
+                    ? CompileAccessorExpression(pkParam, pk[0])
+                    : AnonType.CreateNewExpression(
+                        pk.Select(i => CompileAccessorExpression(pkParam, i)).ToArray(),
+                        _anonTypeNewTweaks
+                    ),
+                pkParam
+            );
+
+            var resultOuterParam = Expression.Parameter(typeof(T), "outer");
+            var resultInnerParam = Expression.Parameter(typeof(T), "inner");
+            var resultLambda = Expression.Lambda(resultOuterParam, resultOuterParam, resultInnerParam);
+
+            return Expression.Call(
+                typeof(Queryable),
+                nameof(Queryable.Join),
+                new[] { typeof(T), typeof(T), pkLambda.ReturnType, typeof(T) },
+                outer, inner,
+                Expression.Quote(pkLambda), Expression.Quote(pkLambda),
+                Expression.Quote(resultLambda)
+            );
+        }
+
+    }
+
+}
