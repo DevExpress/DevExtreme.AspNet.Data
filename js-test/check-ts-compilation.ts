a1eac1610d1f2c306d7ca507481f9625f935c43f
AlekseyMartynov <AlekseyMartynov@users.noreply.github.com>
2019-07-15 16:45:58 +0300

    Actualize d.ts

--- js-test/check-ts-compilation.ts
+++ js-test/check-ts-compilation.ts
@@ -16,3 +16,25 @@ let store = AspNetData.createStore({
 store.load({ sort: "name" }).done(r => {
 
 })
+
+AspNetData.createStore({
+    loadMode: "raw",
+    cacheRawData: false,
+
+    onLoading: (loadOptions) => console.log(loadOptions.filter),
+    onLoaded: (result) => console.log(result.length),
+
+    onInserting: (values) => console.log(values),
+    onInserted: (values, key) => console.log(values, key),
+
+    onUpdating: (key, values) => console.log(key, values),
+    onUpdated: (key, values) => console.log(key, values),
+
+    onRemoving: (key) => console.log(key),
+    onRemoved: (key) => console.log(key),
+
+    onModifying: () => console.log("modifying"),
+    onModified: () => console.log("modified"),
+
+    onPush: (changes) => console.log(changes.length)
+})
