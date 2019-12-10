721e73ad265bcac9247a703c6062fdba40090570
AlekseyMartynov <AlekseyMartynov@users.noreply.github.com>
2019-12-10 16:40:00 +0300

    Lazy adapter

--- net/DevExtreme.AspNet.Data/Async/AsyncHelper.cs
+++ net/DevExtreme.AspNet.Data/Async/AsyncHelper.cs
@@ -9,37 +9,38 @@ namespace DevExtreme.AspNet.Data.Async {
 
     class AsyncHelper {
         readonly IQueryProvider Provider;
-        readonly QueryProviderInfo ProviderInfo;
         readonly CancellationToken CancellationToken;
+        readonly Lazy<IAsyncAdapter> Adapter;
 
         public AsyncHelper(IQueryProvider provider, QueryProviderInfo providerInfo, CancellationToken cancellationToken) {
             Provider = provider;
-            ProviderInfo = providerInfo;
             CancellationToken = cancellationToken;
+
+            IAsyncAdapter CreateAdapter() {
+                var providerType = Provider.GetType();
+
+                var customAdapter = CustomAsyncAdapters.GetAdapter(providerType);
+                if(customAdapter != null)
+                    return customAdapter;
+
+                var reflectionAdapter = new ReflectionAsyncAdapter(providerInfo);
+                if(reflectionAdapter.IsSupportedProvider)
+                    return reflectionAdapter;
+
+                throw ProviderNotSupported(providerType);
+            }
+
+            Adapter = new Lazy<IAsyncAdapter>(CreateAdapter);
         }
 
         public Task<int> CountAsync(Expression expr) {
             CancellationToken.ThrowIfCancellationRequested();
-            return CreateAdapter().CountAsync(Provider, expr, CancellationToken);
+            return Adapter.Value.CountAsync(Provider, expr, CancellationToken);
         }
 
         public Task<IEnumerable<T>> ToEnumerableAsync<T>(Expression expr) {
             CancellationToken.ThrowIfCancellationRequested();
-            return CreateAdapter().ToEnumerableAsync<T>(Provider, expr, CancellationToken);
-        }
-
-        IAsyncAdapter CreateAdapter() {
-            var providerType = Provider.GetType();
-
-            var customAdapter = CustomAsyncAdapters.GetAdapter(providerType);
-            if(customAdapter != null)
-                return customAdapter;
-
-            var reflectionAdapter = new ReflectionAsyncAdapter(ProviderInfo);
-            if(reflectionAdapter.IsSupportedProvider)
-                return reflectionAdapter;
-
-            throw ProviderNotSupported(providerType);
+            return Adapter.Value.ToEnumerableAsync<T>(Provider, expr, CancellationToken);
         }
 
         static Exception ProviderNotSupported(Type providerType) {
