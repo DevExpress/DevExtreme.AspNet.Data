3b72b385a58d08cd6562f07710929a44bcc27928
AlekseyMartynov <AlekseyMartynov@users.noreply.github.com>
2020-12-04 15:38:42 +0300

    Extract template

--- net/DevExtreme.AspNet.Data.Tests/CustomEqualTests.cs
+++ net/DevExtreme.AspNet.Data.Tests/CustomEqualTests.cs
@@ -1,82 +1,61 @@
 ﻿using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace DevExtreme.AspNet.Data.Tests {
 
     public class CustomEqualTests {
 
-        [Theory]
-        [InlineData(false)]
-        [InlineData(true)]
-        public void NoOperator(bool not) {
+        [Fact]
+        public void NoOperator() {
             var source = new[] {
                 new DataItemNoOperator { Value = 1 },
-                new DataItemNoOperator { Value = 2 }
+                new DataItemNoOperator { Value = 2 },
             };
+            var operand = new DataItemNoOperator { Value = 1 };
 
-            var loadOptions = new SampleLoadOptions {
-                GuardNulls = false,
-                Filter = new object[] {
-                    "this",
-                    not ? "<>" : "=",
-                    new DataItemNoOperator { Value = 1 }
-                },
-                IsCountQuery = true
-            };
-
-            var loadResult = DataSourceLoader.Load(source, loadOptions);
-
-            Assert.Equal(1, loadResult.totalCount);
-            Assert.Contains(
-                not
-                    ? ".Where(obj => Not(Equals(obj, {1})))"
-                    : ".Where(obj => Equals(obj, {1}))",
-                loadOptions.ExpressionLog[0]
-            );
+            TestFilter(source, "=", operand, ".Where(obj => Equals(obj, {1}))");
+            TestFilter(source, "<>", operand, ".Where(obj => Not(Equals(obj, {1})))");
         }
 
         [Fact]
         public void WithOperator() {
-            var source = new[] {
-                new DataItemWithOperator { Value = 1 },
-                new DataItemWithOperator { Value = 2 }
-            };
-
-            var loadOptions = new SampleLoadOptions {
-                GuardNulls = false,
-                Filter = new object[] { "this", new DataItemWithOperator { Value = 1 } },
-                IsCountQuery = true
-            };
-
-            var loadResult = DataSourceLoader.Load(source, loadOptions);
-
-            Assert.Equal(1, loadResult.totalCount);
-            Assert.Contains(
-                ".Where(obj => (obj == {1}))",
-                loadOptions.ExpressionLog[0]
+            TestFilter(
+                new[] {
+                    new DataItemWithOperator { Value = 1 },
+                    new DataItemWithOperator { Value = 2 },
+                },
+                "=", new DataItemWithOperator { Value = 1 },
+                ".Where(obj => (obj == {1}))"
             );
         }
 
         [Fact]
         public void Struct() {
-            var source = new[] {
-                new DataItemStruct { Value = 1 },
-                new DataItemStruct { Value = 2 }
-            };
+            TestFilter(
+                new[] {
+                    new DataItemStruct { Value = 1 },
+                    new DataItemStruct { Value = 2 },
+                },
+                "=", new DataItemStruct { Value = 1 },
+                ".Where(obj => Equals("
+                    + Compat.ExpectedConvert("obj", "Object") + ", "
+                    + Compat.ExpectedConvert("{1}", "Object")
+                    + "))"
+            );
+        }
 
+        static void TestFilter<T>(IEnumerable<T> source, string operation, T operand, string expectedExprPart) {
             var loadOptions = new SampleLoadOptions {
                 GuardNulls = false,
-                Filter = new object[] { "this", new DataItemStruct { Value = 1 } },
+                Filter = new object[] { "this", operation, operand },
                 IsCountQuery = true
             };
 
             var loadResult = DataSourceLoader.Load(source, loadOptions);
 
             Assert.Equal(1, loadResult.totalCount);
-            Assert.Contains(
-                ".Where(obj => Equals(" + Compat.ExpectedConvert("obj", "Object") +  ", " + Compat.ExpectedConvert("{1}", "Object") + "))",
-                loadOptions.ExpressionLog[0]
-            );
+            Assert.Contains(expectedExprPart, loadOptions.ExpressionLog[0]);
         }
 
         class DataItemNoOperator {
