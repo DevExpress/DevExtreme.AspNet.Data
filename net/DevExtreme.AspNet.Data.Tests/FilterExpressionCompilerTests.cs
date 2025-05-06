d20adc20c1b538564f56a331f98c83096578d230
Mikhail Preyskurantov <5574159+mpreyskurantov@users.noreply.github.com>
2025-05-06 10:56:51 +0300

    Refactor tests with JsonSerializerOptions (#664)

--- net/DevExtreme.AspNet.Data.Tests/FilterExpressionCompilerTests.cs
+++ net/DevExtreme.AspNet.Data.Tests/FilterExpressionCompilerTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DevExtreme.AspNet.Data.Helpers;
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +11,6 @@ using Xunit;
 namespace DevExtreme.AspNet.Data.Tests {
 
     public class FilterExpressionCompilerTests {
-        static readonly JsonSerializerOptions TESTS_DEFAULT_SERIALIZER_OPTIONS = new JsonSerializerOptions(JsonSerializerDefaults.Web) {
-            Converters = { new ListConverter() }
-        };
 
         class DataItem1 {
             public int IntProp { get; set; }
@@ -146,7 +145,7 @@ namespace DevExtreme.AspNet.Data.Tests {
 
         [Fact]
         public void IsUnaryWithJsonCriteria() {
-            var crit = JsonSerializer.Deserialize<IList>("[\"!\", []]", TESTS_DEFAULT_SERIALIZER_OPTIONS);
+            var crit = JsonSerializer.Deserialize<IList>("[\"!\", []]", DataSourceLoadOptionsParser.DEFAULT_SERIALIZER_OPTIONS);
             var compiler = new FilterExpressionCompiler(typeof(object), false);
             Assert.True(compiler.IsUnary(crit));
         }
@@ -243,7 +242,7 @@ namespace DevExtreme.AspNet.Data.Tests {
 
         [Fact]
         public void JsonObjects() {
-            var crit = JsonSerializer.Deserialize<IList>(@"[ [ ""StringProp"", ""abc"" ], [ ""NullableProp"", null ] ]", TESTS_DEFAULT_SERIALIZER_OPTIONS);
+            var crit = JsonSerializer.Deserialize<IList>(@"[ [ ""StringProp"", ""abc"" ], [ ""NullableProp"", null ] ]", DataSourceLoadOptionsParser.DEFAULT_SERIALIZER_OPTIONS);
             var expr = Compile<DataItem1>(crit);
             Assert.Equal(@"((obj.StringProp == ""abc"") AndAlso (obj.NullableProp == null))", expr.Body.ToString());
         }
