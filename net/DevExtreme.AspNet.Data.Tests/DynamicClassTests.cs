182e124dd5f8a441a11cc5ebf39810d86c2e009c
Alisher Amonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-04-23 16:41:46 +0300

    DataSourceLoader - Update DynamicClassBridge class to use renamed DynamicClass indexer in System.Linq.Dynamic.Core v1.6.8 (#683)
    Co-authored-by: Mikhail Preyskurantov <5574159+mpreyskurantov@users.noreply.github.com>

--- net/DevExtreme.AspNet.Data.Tests/DynamicClassTests.cs
+++ net/DevExtreme.AspNet.Data.Tests/DynamicClassTests.cs
@@ -1,6 +1,11 @@
 ﻿using DevExtreme.AspNet.Data.ResponseModel;
+using DevExtreme.AspNet.Data.Types;
+
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+
 using Xunit;
 
 namespace DevExtreme.AspNet.Data.Tests {
@@ -91,6 +96,43 @@ namespace DevExtreme.AspNet.Data.Tests {
                 line.Contains(".Select(obj => new <>f__AnonymousType") &&
                 line.Contains("I100 = obj.p")
             );
+        }
+
+        [Fact]
+        public void Select_WithMoreThan32Fields_ReturnsValues() {
+            var source = new[] { new { p = 42 } };
+
+            var loadOptions = new SampleLoadOptions {
+                GuardNulls = false,
+                RemoteSelect = true,
+                Select = Enumerable.Repeat("p", 33).ToArray()
+            };
+
+            var loadResult = DataSourceLoader.Load(source, loadOptions);
+            var item = loadResult.data.Cast<IDictionary<string, object>>().First();
+
+            Assert.Equal(42, item["p"]);
+        }
+
+        [Fact]
+        public void DynamicClassBridge_Indexer_GetMember_RoundTripsValue() {
+            var expectedValue = "test";
+            var dynamicType = CallDynamicClassBridgeCreateType(new[] { typeof(string) });
+            var instance = Activator.CreateInstance(dynamicType, expectedValue);
+            Assert.Equal(expectedValue, CallDynamicClassBridgeGetIndexerMember(instance, 0));
+        }
+
+        static MethodInfo GetDynamicClassBridgeMethod(string methodName) {
+            var bridgeType = typeof(AnonType).Assembly.GetType("DevExtreme.AspNet.Data.Types.DynamicClassBridge");
+            return bridgeType.GetMethod(methodName, BindingFlags.Public | BindingFlags.Static);
+        }
+
+        static Type CallDynamicClassBridgeCreateType(Type[] memberTypes) {
+            return (Type)GetDynamicClassBridgeMethod("CreateType").Invoke(null, new object[] { memberTypes });
+        }
+
+        static object CallDynamicClassBridgeGetIndexerMember(object obj, int index) {
+            return GetDynamicClassBridgeMethod("GetMember").Invoke(null, new object[] { obj, index });
         }
 
     }
